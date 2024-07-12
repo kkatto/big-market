51efa57bb30a6fb5a4c287ceed46b37c73e83285
xiaokouxyz <91874335+xiaokouxyz@users.noreply.github.com>
2024-07-12 22:07:43 +0800

    fix:修正概率值计算 StrategyArmoryDispatch#assembleLotteryStrategy

--- big-market-app/src/test/java/com/kou/test/ApiTest.java
+++ big-market-app/src/test/java/com/kou/test/ApiTest.java
@@ -7,13 +7,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @Slf4j
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class ApiTest {
 
     @Test
     public void test() {
-        log.info("测试完成");
+        double x  = 0.0009d;
+
+        System.out.println(convert(x));
+
+    }
+
+    private double convert(double min){
+        double current = min;
+        double max = 1;
+        while (current < 1){
+            current = current * 10;
+            max = max * 10;
+        }
+        return max;
     }
 
 }
