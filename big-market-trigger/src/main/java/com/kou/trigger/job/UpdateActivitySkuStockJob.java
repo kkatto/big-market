c1489fe124d831aec16657c05a6a5699955e577f
xiaokouxyz <91874335+xiaokouxyz@users.noreply.github.com>
2024-09-24 09:31:24 +0800

    feat: xxl-job分布式任务调度

--- big-market-trigger/src/main/java/com/kou/trigger/job/UpdateActivitySkuStockJob.java
+++ big-market-trigger/src/main/java/com/kou/trigger/job/UpdateActivitySkuStockJob.java
@@ -2,13 +2,17 @@ package com.kou.trigger.job;
 
 import com.kou.domain.activity.model.valobj.ActivitySkuStockKeyVO;
 import com.kou.domain.activity.service.IRaffleActivitySkuStockService;
+import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author KouJY
@@ -23,11 +27,24 @@ public class UpdateActivitySkuStockJob {
     private IRaffleActivitySkuStockService skuStock;
     @Resource
     private ThreadPoolExecutor executor;
+    @Resource
+    private RedissonClient redissonClient;
 
-    @Scheduled(cron = "0/5 * * * * ?")
+    /**
+     * 本地化任务注解；@Scheduled(cron = "0/5 * * * * ?")
+     * 分布式任务注解；@XxlJob("SendMessageTaskJob")
+     */
+    @XxlJob("UpdateActivitySkuStockJob")
     public void exec() {
+        // 为什么加锁？分布式应用N台机器部署互备，任务调度会有N个同时执行，那么这里需要增加抢占机制，谁抢占到谁就执行。完毕后，下一轮继续抢占。
+        RLock lock = redissonClient.getLock("big-market-UpdateActivitySkuStockJob");
+        boolean isLocked = false;
         try {
-            //log.info("定时任务，更新活动sku库存【延迟队列获取，降低对数据库的更新频次，不要产生竞争】");
+            isLocked = lock.tryLock(3, 0, TimeUnit.SECONDS);
+            if (!isLocked) {
+                return;
+            }
+
             List<Long> skuList = skuStock.querySkuList();
 
             for (Long sku : skuList) {
@@ -47,6 +64,10 @@ public class UpdateActivitySkuStockJob {
             }
         } catch (Exception e) {
             log.error("定时任务，更新活动sku库存失败", e);
+        } finally {
+            if (isLocked) {
+                lock.unlock();
+            }
         }
     }
 }
--- big-market-trigger/src/main/java/com/kou/trigger/job/UpdateAwardStockJob.java
+++ big-market-trigger/src/main/java/com/kou/trigger/job/UpdateAwardStockJob.java
@@ -3,19 +3,23 @@ package com.kou.trigger.job;
 import com.kou.domain.strategy.model.valobj.StrategyAwardStockKeyVO;
 import com.kou.domain.strategy.service.IRaffleAward;
 import com.kou.domain.strategy.service.IRaffleStock;
+import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author KouJY
  * Date: 2024/7/13 11:43
  * Package: com.kou.trigger.job
- *
+ * <p>
  * 更新奖品库存任务；为了不让更新库存的压力打到数据库中，这里采用了redis更新缓存库存，异步队列更新数据库，数据库表最终一致即可。
  */
 @Slf4j
@@ -28,13 +32,27 @@ public class UpdateAwardStockJob {
     private IRaffleAward raffleAward;
     @Resource
     private ThreadPoolExecutor executor;
+    @Resource
+    private RedissonClient redissonClient;
 
-    @Scheduled(cron = "0/5 * * * * ?")
+    /**
+     * 本地化任务注解；@Scheduled(cron = "0/5 * * * * ?")
+     * 分布式任务注解； @XxlJob("updateAwardStockJob")
+     */
+    @XxlJob("updateAwardStockJob")
     public void exec() {
+        // 为什么加锁？分布式应用N台机器部署互备，任务调度会有N个同时执行，那么这里需要增加抢占机制，谁抢占到谁就执行。完毕后，下一轮继续抢占。
+        RLock lock = redissonClient.getLock("big-market-updateAwardStockJob");
+        boolean isLocked = false;
         try {
-            List<StrategyAwardStockKeyVO> strategyAwardStockKeyVOS = raffleAward.queryOpenActivityStrategyAwardList();
-            if (null == strategyAwardStockKeyVOS) return;
-            for (StrategyAwardStockKeyVO strategyAwardStockKeyVO : strategyAwardStockKeyVOS) {
+            isLocked = lock.tryLock(3, 0, TimeUnit.SECONDS);
+            if (!isLocked) return;
+
+            List<StrategyAwardStockKeyVO> strategyAwardStockKeyVOList = raffleAward.queryOpenActivityStrategyAwardList();
+            if (null == strategyAwardStockKeyVOList) {
+                return;
+            }
+            for (StrategyAwardStockKeyVO strategyAwardStockKeyVO : strategyAwardStockKeyVOList) {
                 executor.execute(() -> {
                     try {
                         StrategyAwardStockKeyVO queueStrategyAwardStockKeyVO = raffleStock.takeQueueValue(strategyAwardStockKeyVO.getStrategyId(), strategyAwardStockKeyVO.getAwardId());
@@ -48,6 +66,10 @@ public class UpdateAwardStockJob {
             }
         } catch (Exception e) {
             log.error("定时任务，更新奖品消耗库存失败", e);
+        } finally {
+            if (isLocked) {
+                lock.unlock();
+            }
         }
     }
 }
