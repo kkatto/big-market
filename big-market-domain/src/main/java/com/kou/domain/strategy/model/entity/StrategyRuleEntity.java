5dcdb3907c48289a91dab3d58440f0d4f0d2365c
xiaokouxyz <91874335+xiaokouxyz@users.noreply.github.com>
2025-03-06 16:16:44 +0800

    feat: 监控组件接入 - 部署线上 - 修改抽奖阶梯不按照顺序

--- big-market-domain/src/main/java/com/kou/domain/strategy/model/entity/StrategyRuleEntity.java
+++ big-market-domain/src/main/java/com/kou/domain/strategy/model/entity/StrategyRuleEntity.java
@@ -6,10 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author KouJY
@@ -51,7 +48,7 @@ public class StrategyRuleEntity {
             return null;
         }
         String[] ruleValueMaps = this.ruleValue.split(Constants.SPACE);
-        Map<String, List<Integer>> resultMap = new HashMap<>();
+        Map<String, List<Integer>> resultMap = new LinkedHashMap<>();
 
         for (String ruleValueMap : ruleValueMaps) {
             // 检查是否为空
